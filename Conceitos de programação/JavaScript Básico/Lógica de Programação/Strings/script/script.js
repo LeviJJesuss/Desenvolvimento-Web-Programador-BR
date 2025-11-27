581986c537e5474d0b8c194c5f890b401f1e1c4e
LeviJJesuss <jardimlevi10@gmail.com>
2025-11-27 16:14:23 -0300

    JS Básico/Strings

---
+++ Conceitos de programação/JavaScript Básico/Lógica de Programação/Strings/script/script.js
@@ -0,0 +1,9 @@
+// Posso criar uma string tanto com aspas duplas como aspas simples
+// Se eu quiser usar aspas dentro do meu texto eu utilizo ou simples ou dupla no texto
+// Eu posso usar operação de adição com uma string
+// NaN (NOT AN NUMBER)
+
+var str = "Valor qualquer";
+str += ", outro texto";
+
+console.log(str);
