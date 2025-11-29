07ac415272edc5bd38fece6c8e8248e752332c78
LeviJJesuss <jardimlevi10@gmail.com>
2025-11-28 22:56:32 -0300

    Desafio calculando média escolar

---
+++ Conceitos de programação/JavaScript Básico/Lógica de Programação/Desafio Escola/script.js
@@ -0,0 +1,38 @@
+// Eu quero ter o nome do aluno separado pela nota1 e nota2 sua média
+// e se ele foi aprovado ou reprovado
+
+var nomes = ["Igor", "José", "Maria"];
+var notasA = [7.0, 6.5, 9.5];
+var notasB = [8.0, 7.0, 8.5];
+
+function media(n1, n2) {
+    return (n1 + n2) / 2
+}
+
+function passou(media) {
+    // var resultado = "Reprovado";
+    // if(media > 7) {
+    //     resultado = "Aprovado"
+    // }
+    // return resultado;
+    if(media > 7) {
+        return "Aprovado"
+    } else {
+        return "Reprovado"
+    }
+}
+
+for(var index in nomes) {
+    var nota1 = notasA[index];
+    var nota2 = notasB[index];
+
+    var m = media(nota1, nota2);
+
+    console.log(nomes[index] +
+                    " - " +
+                    nota1 + 
+                    " - " +
+                    nota2 +
+                    " - " +
+                    passou(m));
+}
